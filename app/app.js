a66787ff707bbfb8b4cd42098341c3f6e32ee059
xuejiang <catcxj@163.com>
2016-06-21 11:30:47 +0800

    merge

--- app/app.js
+++ app/app.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyRouterMiddleware, browserHistory, Router } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import useScroll from 'react-router-scroll';
+import configureStore from './store';
 
-import { Router, Route } from 'react-router';
 
-React.render((
-	<Router>
-		<Route path="/" component={App}>
-			<Route path="about" component={About} />
-			<Route path="inbox" component={Inbox} />
-		</Route>
-	</Router>
-), document.body);
+const initialState = {};
+const store = configureStore(initialState, browserHistory);
+
+if (window.devToolsExtension) {
+	window.devToolsExtension.updateStore(store);
+}
+
+import { selectLocationState } from 'containers/App/selectors';
+const history = syncHistoryWithStore(browserHistory, store, {
+	selectLocationState: selectLocationState(),
+});
+
+import App from 'containers/App';
+import createRoutes from './routes';
+const rootRoute = {
+	component: App,
+	childRoutes: createRoutes(store),
+};
+
+ReactDOM.render((
+	<Provider store={store}>
+		<Router history={history} 
+			routes={rootRoute} 
+			render={
+				applyRouterMiddleware(useScroll())
+			}
+		/>
+	</Provider>
+), document.getElementById('app'));
+
+
+import { install } from 'offline-plugin/runtime';
+install();
---
+++ app/store.js
@@ -0,0 +1,43 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { fromJS } from 'immutable';
+import { routerMiddleware } from 'react-router-redux';
+import createSagaMiddleware from 'redux-saga';
+import createReducer from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
+const devtools = window.devToolsExtension || (() => noop => noop);
+
+export default function configureStore(initialState = {}, history) {
+	const middlewares = [
+		sagaMiddleware,
+		routerMiddleware(history),
+	];
+
+	const enhancers = [
+		applyMiddleware(...middlewares),
+		devtools(),
+	];
+
+	const store = createStore(
+		createReducer(),
+		fromJS(initialState),
+		compose(...enhancers)
+	);
+
+	store.runSaga = sagaMiddleware.run;
+	store.asyncReducers = {};
+
+
+	if (module.hot) {
+		module.hot.accept('./reducers', () => {
+			System.import('./reducers').then((reducerModule) => {
+				const createReducers = reducerModule.default;
+				const nextReducers = createReducers(store.asyncReducers);
+
+				store.replaceReducer(nextReducers);
+			});
+		});
+	}
+
+	return store;
+}
